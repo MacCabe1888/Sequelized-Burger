d840ba2a82c71adb21c1c66f5b63d99d66a1aad3
Cameron <cameronjmaccabe@gmail.com>
2019-04-22 05:27:56 -0700

    added models

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,13 @@
-// Import the ORM to create functions that will interact with the database.
-const orm = require("../config/orm");
-
-const burger = {
-  all: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(columns, values, cb) {
-    orm.insertOne("burgers", columns, values, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.deleteOne("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Burger = sequelize.define('Burger', {
+    burger_name: {
+      type: DataTypes.STRING
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Burger;
 };
-
-// Export the database functions for the controller ("burgers_controller.js").
-module.exports = burger;
