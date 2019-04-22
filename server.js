116f193f905e08d1c621e563b80a5e6b64fe2f35
Cameron <cameronjmaccabe@gmail.com>
2019-04-22 05:35:55 -0700

    set up server to sync Sequelize models

--- server.js
+++ server.js
@@ -4,26 +4,28 @@ const PORT = process.env.PORT || 8129;
 
 const app = express();
 
+// Require our models for syncing.
+const db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body
+// Parse application body.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 const routes = require("./controllers/burgers_controller");
-
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Sync our sequelize models and then start our Express app.
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
